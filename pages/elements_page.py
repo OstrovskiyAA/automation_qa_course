6000b6180d2d96080edf7526f71f5f26c4a303b2
Aleksei Ostrovskii <a.a.ostrovskiy@mail.ru>
2024-04-13 15:34:41 +0300

    4.1 lessons + randomizer and notice about failt

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -1,13 +1,20 @@
+from generation.generator import generated_person
 from locators.elements_page_locators import TextBoxPageLocators
 from pages.base_page import BasePage
 
 class TextBoxPage(BasePage):
     locators = TextBoxPageLocators()
     def fill_all_fields(self):
-        self.element_is_visible(self.locators.FULL_NAME).send_keys('jkhkjh')
-        self.element_is_visible(self.locators.EMAIL).send_keys('sdsdsd@sjdhjs.com')
-        self.element_is_visible(self.locators.CURRENT_ADDRESS).send_keys('sdsd')
-        self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys('sdsds')
+        person_info = next(generated_person())
+        full_name = person_info.full_name
+        email = person_info.email
+        current_address = person_info.current_address
+        permanent_address = person_info.permanent_address
+        self.element_is_visible(self.locators.FULL_NAME).send_keys(full_name)
+        self.element_is_visible(self.locators.EMAIL).send_keys(email)
+        self.element_is_visible(self.locators.CURRENT_ADDRESS).send_keys(current_address)
+        self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys(permanent_address)
+        return full_name, email, current_address, permanent_address
     def click_submit(self):
         self.element_is_clickable(self.locators.SUBMIT).click()
 
--- tests/elements_test.py
+++ tests/elements_test.py
@@ -16,16 +16,18 @@ class TestTextBox:
     def test_text_box(self, driver):
         test_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
         test_box_page.open()
-        test_box_page.fill_all_fields()
+        # input_data = test_box_page.fill_all_fields()
+        full_name, email, current_address, permanent_address = test_box_page.fill_all_fields()
         test_box_page.scroll_down()
         test_box_page.click_submit()
         output_name, output_email, output_current_address, output_permanent_address = test_box_page.check_filled_form()
         print()
-        print(output_name)
-        print(output_email)
-        print(output_current_address)
-        print(output_permanent_address)
-        # print(test_box_page.check_filled_form()) - первый вариант вывода информации
-        time.sleep(2)
+        assert full_name == output_name, "the full name doesn't match"
+        assert email == output_email, "the full name doesn't match"
+        assert current_address == output_current_address, "the full name doesn't match"
+        assert permanent_address == output_permanent_address, "the full name doesn't match"
+        # output_data = test_box_page.check_filled_form()
+        # assert input_data == output_data
+        time.sleep(5)
 
 
